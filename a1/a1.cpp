a42fe311c969a40260d4d5dd677d262eb4dc0e4c
semiartist <semiartist@ymail.com>
2016-12-10 15:19:40 -0500

    add a1

---
+++ a1/a1.cpp
@@ -0,0 +1,13 @@
+// File: a1.cpp
+// Author: Frank Chen fchen29
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char *argv[]) {
+    
+}
